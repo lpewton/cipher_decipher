662cf8631118f43f1a51833ecff77c40f1536202
lpewton <liampewton99@gmail.com>
2023-12-01 03:15:24 +0000

    Create random letter string

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// Makes sure there is a value in Input
+
+let allLetters = 'Jinxedwzarspluckvyfombgqt'
+console.log(allLetters.length)
 let processInput = () => {
     let input = document.getElementById('cipher-input').value;
-    console.log('Hello')
     if (input) {
         return input;
     } else {
@@ -9,5 +10,36 @@ let processInput = () => {
     }
 }
 
+let randomiseCode = () => {
+    let input = processInput();
+    let encoded = []
+    let usedIndexes = []
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] === ' ') {
+            encoded[i] = ' ';
+            continue;
+        }
+        if (usedIndexes.includes(i)) {
+            continue;
+        }
+
+        getRandomLetter()
+        encoded[i] = (allLetters[randomLetter])
+
+        for (let j = input.length; j > i; j--) {
+            if (input[i] === input[j]) {
+                encoded[j] = allLetters[randomLetter]
+                usedIndexes.push(j)
+            }
+        }
+    }
+    let ciperedCode = document.getElementsByClassName('ciphered-code')[0];
+    ciperedCode.innerHTML = encoded.join('');
+    return encoded;
+}
+
+let getRandomLetter = () => randomLetter = Math.floor(Math.random() * 24);
+console.log(allLetters)
 let cipherButton = document.getElementById('cipher-btn')
-cipherButton.addEventListener('click', processInput);
+cipherButton.addEventListener('click', randomiseCode);
